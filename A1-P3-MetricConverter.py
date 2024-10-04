eeb86f5a166f76cf73b96474cebb866dda690823
w0425777 <w0425777@nscc.ca>
2024-10-04 13:55:57 -0300

    Finished

--- A1-P3-MetricConverter.py
+++ A1-P3-MetricConverter.py
@@ -4,13 +4,35 @@
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
+    print("\nImperial to Metric Conversion\n")
+    #pritn title
 
+    tons = float(input("Enter the number of tons: "))
+    #input tones,stones,pounds ounces(make sure to convert to float or int)
 
+    stones = float(input("Enter the number of stones: "))
 
+    pounds = float(input("Enter the of pounds: "))
 
+    ounces = float(input("Enter the number of Ounces: "))
 
+    tOunces = 35840*tons+224*stones+16*pounds+ounces
+    #equation for Total Ounces
 
+    tKilos = (tOunces/35.274)
+    #equation for Total Kilos
 
+    mTons = int(tKilos/1000)
+    #equation for Total Tons
+
+    mKilos = int(tOunces/35.274)-(mTons*1000)
+    #Make new value to - tons form kilos
+
+    grams = (tKilos-(int(tOunces/35.274)))*1000
+    #equation for grams???
+
+    print("\nThe metric weight is {0} metric tons, {1} kilos, and {2:.1f} grams.\n".format(mTons,mKilos,grams))
+    #Pray that it works
 
     # YOUR CODE ENDS HERE
 
